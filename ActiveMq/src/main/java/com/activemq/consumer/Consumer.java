3984a73625c5582a1e223c59deddefec44e3b697
sfjbssept <sfjbssept@gmail.com>
2022-10-10 04:05:25 +0000

    committing consumer

---
+++ ActiveMq/src/main/java/com/activemq/consumer/Consumer.java
@@ -0,0 +1,14 @@
+package com.activemq.consumer;
+
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Consumer {
+
+	@JmsListener(destination = "standalone-queue")
+	public void consume(String msg) {
+		System.out.println("Hi , it is consumed");
+		
+	}
+}
